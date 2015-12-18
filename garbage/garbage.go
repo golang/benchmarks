5137d16ec859718fb0eb3e67f320edb0820bc0d2
Austin Clements <austin@google.com>
2015-12-18 14:54:46 -0800

    garbage: used a fixed copy of the net/http source
    Currently garbage parses whatever source is in $GOROOT/src/net/http,
    but this means that the expected performance of the benchmark changes
    over time as net/http changes. Indeed, net/http has nearly doubled in
    size between Go 1.5 and current master, and this can be seen in the
    performance of the benchmark:
    
                   Build version
    net/http   go1.5.2     ab74f59
    go1.5.2   10.8 ms/op  10.1 ms/op
    ab74f59   19.9 ms/op  18.5 ms/op
    
    If you compare garbage built at go1.5.2, running with a go1.5.2 source
    tree against garbage built at ab74f59, running with an ab74f59 source
    tree, it will appear that the benchmark has slowed down by 71%. But,
    in fact, if you hold the source tree constant, it has sped up by 7%.
    
    Fix this by bundling a static snapshot of net/http, taken at git tag
    go1.5.2, in to the garbage benchmark and parsing that instead of
    whatever's in GOROOT.
    
    Change-Id: If86b67f37ca33d275d4bb7168ee8cc5f5227ee6f
    Reviewed-on: https://go-review.googlesource.com/18061
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- garbage/garbage.go
+++ garbage/garbage.go
@@ -6,16 +6,15 @@
 // It repeatedly parses net/http package with go/parser and then discards results.
 package garbage // import "golang.org/x/benchmarks/garbage"
 
+// The source of net/http was captured at git tag go1.5.2 by
+//go:generate sh -c "(echo 'package garbage'; echo 'var src = `'; bundle net/http http '' | sed 's/`/`+\"`\"+`/g'; echo '`') > nethttp.go"
+
 import (
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"os"
-	"path"
-	"path/filepath"
 	"runtime"
-	"strings"
 	"sync"
 	"sync/atomic"
 
@@ -26,7 +25,7 @@ func init() {
 	driver.Register("garbage", benchmark)
 }
 
-type ParsedPackage map[string]*ast.Package
+type ParsedPackage *ast.File
 
 var (
 	parsed []ParsedPackage
@@ -107,57 +106,10 @@ func packageMemConsumption() int {
 
 // parsePackage parses and returns net/http package.
 func parsePackage() ParsedPackage {
-	pkgname := "http"
-	dirpath := filepath.Join(runtime.GOROOT(), "/src/pkg/net/", pkgname)
-	if !exists(dirpath) {
-		// As of 8th Sept 2014 the "pkg" prefix was removed from the std lib
-		// http://golang.org/s/go14nopkg
-		dirpath = filepath.Join(runtime.GOROOT(), "/src/net/", pkgname)
-	}
-	// filter function to select the desired .go files
-	filter := func(d os.FileInfo) bool {
-		if isPkgFile(d) {
-			// Some directories contain main packages: Only accept
-			// files that belong to the expected package so that
-			// parser.ParsePackage doesn't return "multiple packages
-			// found" errors.
-			// Additionally, accept the special package name
-			// fakePkgName if we are looking at cmd documentation.
-			name := pkgName(dirpath + "/" + d.Name())
-			return name == pkgname
-		}
-		return false
-	}
-
-	// get package AST
-	pkgs, err := parser.ParseDir(token.NewFileSet(), dirpath, filter, parser.ParseComments)
+	pkgs, err := parser.ParseFile(token.NewFileSet(), "net/http", src, parser.ParseComments)
 	if err != nil {
-		println("parse", dirpath, err.Error())
+		println("parse", err.Error())
 		panic("fail")
 	}
 	return pkgs
-}
-
-func isGoFile(dir os.FileInfo) bool {
-	return !dir.IsDir() &&
-		!strings.HasPrefix(dir.Name(), ".") && // ignore .files
-		path.Ext(dir.Name()) == ".go"
-}
-
-func isPkgFile(dir os.FileInfo) bool {
-	return isGoFile(dir) &&
-		!strings.HasSuffix(dir.Name(), "_test.go") // ignore test files
-}
-
-func pkgName(filename string) string {
-	file, err := parser.ParseFile(token.NewFileSet(), filename, nil, parser.PackageClauseOnly)
-	if err != nil || file == nil {
-		return ""
-	}
-	return file.Name.Name
-}
-
-func exists(path string) bool {
-	_, err := os.Stat(path)
-	return !os.IsNotExist(err)
 }
