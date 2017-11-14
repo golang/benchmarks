1a661c9c17c0675fcdaaa6725811d36636053752
Tobias Klauser <tklauser@distanz.ch>
2017-11-14 12:18:39 +0100

    driver: allow to build for all BSDs
    Add appropriate build tags in order to allow building the driver for all
    BSDs supported by Go.
    
    Change-Id: I9f97051c6dbd85a8c00798cfa94fed90504c4fcb
    Reviewed-on: https://go-review.googlesource.com/77472
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- driver/driver_bsd.go
+++ driver/driver_bsd.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//+build dragonfly freebsd netbsd openbsd
+
 package driver
 
 const rssMultiplier = 1
