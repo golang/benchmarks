c5c50407248a0dd4712c9818a8580bcaff350d68
Austin Clements <austin@google.com>
2024-07-19 09:15:56 -0400

    sweet/benchmarks/internal/server: delete now-unused code
    Change-Id: I5770f324363d8c02987e7f99164be9db91bc4f68
    Reviewed-on: https://go-review.googlesource.com/c/benchmarks/+/600066
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>

--- sweet/benchmarks/internal/server/server.go
+++ sweet/benchmarks/internal/server/server.go
@@ -11,7 +11,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -121,72 +120,4 @@ func collectTo(ctx context.Context, host string, diag *driver.Diagnostics, typ d
 		f.Commit()
 	}
 	return err
-}
-
-// TODO: Delete below here
-
-func CollectDiagnostic(host, tmpDir, benchName string, typ diagnostics.Type) (int64, error) {
-	// We attempt to use the benchmark name to create a temp file so replace all
-	// path separators with "_".
-	benchName = strings.Replace(benchName, "/", "_", -1)
-	benchName = strings.Replace(benchName, string(os.PathSeparator), "_", -1)
-	f, err := os.CreateTemp(tmpDir, benchName+"."+string(typ))
-	if err != nil {
-		return 0, err
-	}
-	defer f.Close()
-	resp, err := http.Get(fmt.Sprintf("http://%s/debug/pprof/%s", host, endpoint(typ)))
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	n, err := io.Copy(f, resp.Body)
-	if err != nil {
-		return 0, err
-	}
-	return n, driver.CopyDiagnosticData(f.Name(), typ, benchName)
-}
-
-func endpoint(typ diagnostics.Type) string {
-	switch typ {
-	case diagnostics.CPUProfile:
-		return "profile?seconds=1"
-	case diagnostics.MemProfile:
-		return "heap"
-	case diagnostics.Trace:
-		return "trace?seconds=1"
-	}
-	panic("diagnostic " + string(typ) + " has no endpoint")
-}
-
-func PollDiagnostic(host, tmpDir, benchName string, typ diagnostics.Type) (stop func() uint64) {
-	// TODO(mknyszek): This is kind of a hack. We really should find a way to just
-	// enable diagnostic collection at a lower level for the entire server run.
-	var stopc chan struct{}
-	var wg sync.WaitGroup
-	var size uint64
-	wg.Add(1)
-	stopc = make(chan struct{})
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-stopc:
-				return
-			default:
-			}
-			n, err := CollectDiagnostic(host, tmpDir, benchName, typ)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "failed to read diagnostic %s: %v", typ, err)
-				return
-			}
-			size += uint64(n)
-		}
-	}()
-	return func() uint64 {
-		// Stop the loop.
-		close(stopc)
-		wg.Wait()
-		return size
-	}
 }
