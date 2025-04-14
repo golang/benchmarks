0bd3073f9ad343a37c7e78084f1c71dc8418d1f8
Michael Pratt <mpratt@google.com>
2025-04-14 10:06:56 -0400

    sweet: don't build tsgo on go1.23
    tsgo requires 1.24, so don't attempt to build it if the test toolchain
    is older than 1.24.
    
    To do this, we extract the version from the toolchain and check that it
    is new enough, which is more difficult than it should be due to #73369.
    
    I suspect generally there is a way to do this with go list, but 1.23
    fails to even parse go.mod due to a tool directive before it can decide
    it is too old to build.
    
    Fixes golang/go#73052.
    Fixes golang/go#73049.
    Fixes golang/go#73047.
    
    Change-Id: I6a6a636c495b52cff1a6196ffee79b44aaa687ba
    Reviewed-on: https://go-review.googlesource.com/c/benchmarks/+/665355
    Reviewed-by: Michael Knyszek <mknyszek@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Michael Pratt <mpratt@google.com>

--- sweet/harnesses/go-build.go
+++ sweet/harnesses/go-build.go
@@ -5,9 +5,12 @@
 package harnesses
 
 import (
+	"debug/buildinfo"
 	"fmt"
+	"go/version"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/benchmarks/sweet/common"
 	"golang.org/x/benchmarks/sweet/common/fileutil"
@@ -17,6 +20,7 @@ import (
 type buildBenchmark struct {
 	name  string
 	pkg   string
+	minGo string // skip if toolchain is older than this
 	clone func(outDir string) error
 }
 
@@ -60,8 +64,9 @@ var (
 			// Added for #72815. This codebase has at least a few packages
 			// that are difficult for the Go compiler to handle, performance-wise,
 			// as of Mar. 13 2025.
-			name: "tsgo",
-			pkg:  "cmd/tsgo",
+			name:  "tsgo",
+			pkg:   "cmd/tsgo",
+			minGo: "go1.24",
 			clone: func(outDir string) error {
 				return gitCloneToCommit(
 					outDir,
@@ -85,8 +90,13 @@ func (h GoBuild) CheckPrerequisites() error {
 }
 
 func (h GoBuild) Get(gcfg *common.GetConfig) error {
+	benchmarks, err := goBuildBenchmarks(nil, gcfg.Short)
+	if err != nil {
+		return fmt.Errorf("error getting benchmark list: %v", err)
+	}
+
 	// Clone the sources that we're going to build.
-	for _, bench := range goBuildBenchmarks(gcfg.Short) {
+	for _, bench := range benchmarks {
 		if err := bench.clone(filepath.Join(gcfg.SrcDir, bench.name)); err != nil {
 			return err
 		}
@@ -97,9 +107,6 @@ func (h GoBuild) Get(gcfg *common.GetConfig) error {
 func (h GoBuild) Build(pcfg *common.Config, bcfg *common.BuildConfig) error {
 	// Local copy of config for updating GOROOT.
 	cfg := pcfg.Copy()
-
-	// Get the benchmarks we're going to build.
-	benchmarks := goBuildBenchmarks(bcfg.Short)
 
 	// cfg.GoRoot is our source toolchain. We need to rebuild cmd/compile
 	// and cmd/link with cfg.BuildEnv to apply any configured build options
@@ -113,6 +120,12 @@ func (h GoBuild) Build(pcfg *common.Config, bcfg *common.BuildConfig) error {
 	cfg.GoRoot = goroot
 	if err := cfg.GoTool().Do("", "install", "cmd/compile", "cmd/link"); err != nil {
 		return fmt.Errorf("error building cmd/compile and cmd/link: %v", err)
+	}
+
+	// Get the benchmarks we're going to build.
+	benchmarks, err := goBuildBenchmarks(cfg, bcfg.Short)
+	if err != nil {
+		return fmt.Errorf("error getting benchmark list: %v", err)
 	}
 
 	for _, bench := range benchmarks {
@@ -153,7 +166,11 @@ func (h GoBuild) Run(pcfg *common.Config, rcfg *common.RunConfig) error {
 	cfg := pcfg.Copy()
 	cfg.GoRoot = filepath.Join(rcfg.BinDir, "goroot") // see Build, above.
 
-	benchmarks := goBuildBenchmarks(rcfg.Short)
+	benchmarks, err := goBuildBenchmarks(cfg, rcfg.Short)
+	if err != nil {
+		return fmt.Errorf("error getting benchmark list: %v", err)
+	}
+
 	for _, bench := range benchmarks {
 		cmd := exec.Command(
 			filepath.Join(rcfg.BinDir, "go-build-bench"),
@@ -174,9 +191,41 @@ func (h GoBuild) Run(pcfg *common.Config, rcfg *common.RunConfig) error {
 	return nil
 }
 
-func goBuildBenchmarks(short bool) []*buildBenchmark {
-	if short {
-		return buildBenchmarksShort
+// goBuildBenchmarks returns the set of benchmarks to run for this
+// configuration.
+//
+// cfg may be nil if the build configuration isn't known yet. In that case, it
+// returns the set of benchmarks that may run.
+func goBuildBenchmarks(cfg *common.Config, short bool) ([]*buildBenchmark, error) {
+	var bi *buildinfo.BuildInfo
+	if cfg != nil {
+		var err error
+		bi, err = buildinfo.ReadFile(cfg.GoTool().Tool)
+		if err != nil {
+			return nil, fmt.Errorf("error reading build info from Go toolchain: %v", err)
+		}
+
+		// NOTE(go.dev/issue/73369): Tip toolchain versions look like
+		// "devel go1.25-9ce47e66e8 Wed Mar 26 03:48:50 2025 -0700".
+		// This is not a valid go/version version, so it sorts before
+		// every valid version. If we strip "devel", then the remainder
+		// is valid.
+		bi.GoVersion = strings.TrimPrefix(bi.GoVersion, "devel ")
 	}
-	return buildBenchmarks
+
+	base := buildBenchmarks
+	if short {
+		base = buildBenchmarksShort
+	}
+
+	var out []*buildBenchmark
+	for _, bench := range base {
+		if bench.minGo != "" && bi != nil && version.Compare(bi.GoVersion, bench.minGo) < 0 {
+			log.Printf("Skipping go-build on %s: toolchain version %s less than required %s", bench.name, bi.GoVersion, bench.minGo)
+			continue
+		}
+		out = append(out, bench)
+	}
+
+	return out, nil
 }
